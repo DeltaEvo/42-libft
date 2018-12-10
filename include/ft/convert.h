84bfe972a309bf9e3448e04783df68d59d32973e
David DE JESUS DUARTE <dde-jesu@student.42.fr>
2018-12-10 15:19:43 +0100

    Remove signed version of str8 and str16 convert

--- include/ft/convert.h
+++ include/ft/convert.h
@@ -6,7 +6,7 @@
 /*   By: dde-jesu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 11:54:58 by dde-jesu          #+#    #+#             */
-/*   Updated: 2018/12/04 11:41:30 by dde-jesu         ###   ########.fr       */
+/*   Updated: 2018/12/10 13:00:42 by dde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,6 @@ typedef struct	s_int_str8 {
 
 t_int_str		ft_int_to_str(intmax_t nb);
 t_int_str		ft_uint_to_str(uintmax_t nb);
-t_int_str16		ft_int_to_str16(intmax_t nbm, uint8_t maj);
 t_int_str16		ft_uint_to_str16(uintmax_t nb, uint8_t maj);
-t_int_str8		ft_int_to_str8(intmax_t nb);
 t_int_str8		ft_uint_to_str8(uintmax_t nb);
 #endif
--- src/convert/int_to_str.c
+++ src/convert/int_to_str.c
@@ -6,7 +6,7 @@
 /*   By: dde-jesu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 10:12:17 by dde-jesu          #+#    #+#             */
-/*   Updated: 2018/12/05 14:12:44 by dde-jesu         ###   ########.fr       */
+/*   Updated: 2018/12/10 13:00:25 by dde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,38 +63,10 @@ t_int_str16	ft_uint_to_str16(uintmax_t nb, uint8_t maj)
 	return (res);
 }
 
-t_int_str16	ft_int_to_str16(intmax_t nb, uint8_t maj)
-{
-	t_int_str16		res;
-
-	ft_uint_to_str_p((uintmax_t)(nb < 0 ? -nb : nb), res.str + (nb < 0),
-			&res.len, maj ? -16 : 16);
-	if (nb < 0)
-	{
-		*res.str = '-';
-		res.len++;
-	}
-	return (res);
-}
-
 t_int_str8	ft_uint_to_str8(uintmax_t nb)
 {
 	t_int_str8	res;
 
 	ft_uint_to_str_p(nb, res.str, &res.len, 8);
-	return (res);
-}
-
-t_int_str8	ft_int_to_str8(intmax_t nb)
-{
-	t_int_str8		res;
-
-	ft_uint_to_str_p((uintmax_t)(nb < 0 ? -nb : nb), res.str + (nb < 0),
-			&res.len, 8);
-	if (nb < 0)
-	{
-		*res.str = '-';
-		res.len++;
-	}
 	return (res);
 }
