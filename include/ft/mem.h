9bac7cefd5bafc78b6a1e10b32c992779c122bc5
David DE JESUS DUARTE <dde-jesu@student.42.fr>
2018-12-12 14:14:39 +0100

    Add ft_memset prototype

--- include/ft/mem.h
+++ include/ft/mem.h
@@ -6,7 +6,7 @@
 /*   By: dde-jesu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 12:59:12 by dde-jesu          #+#    #+#             */
-/*   Updated: 2018/12/05 14:11:03 by dde-jesu         ###   ########.fr       */
+/*   Updated: 2018/12/12 13:04:38 by dde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_realloc(void *o_ptr, size_t oldsize, size_t newsize);
 void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
 
 #endif
