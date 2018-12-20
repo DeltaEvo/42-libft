cc8b4e0096ca53e00807e332aef26a9c1a721723
David DE JESUS DUARTE <dde-jesu@student.42.fr>
2018-12-20 11:50:29 +0100

    Norm

--- include/ft/io.h
+++ include/ft/io.h
@@ -6,7 +6,7 @@
 /*   By: dde-jesu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 12:11:03 by dde-jesu          #+#    #+#             */
-/*   Updated: 2018/12/17 12:25:35 by dde-jesu         ###   ########.fr       */
+/*   Updated: 2018/12/20 11:49:28 by dde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,8 @@ typedef	struct	s_writable {
 typedef ssize_t	(*t_io_fill)(t_readable *r);
 typedef ssize_t	(*t_io_flush)(t_writable *r);
 
-t_readable		init_readable(t_io_fill fill, void *data, uint8_t *buff, size_t s);
+t_readable		init_readable(t_io_fill fill, void *data, uint8_t *buff,
+		size_t s);
 t_writable		init_writable(t_io_flush flush, void *data);
 ssize_t			io_write(t_writable *w, char data[], size_t len);
 ssize_t			io_read(t_readable *r, char data[], size_t len);
